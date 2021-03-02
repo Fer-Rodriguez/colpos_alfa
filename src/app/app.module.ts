050374285558c161b6af03d57c3027072a0bf574
Karen Rugerio <A01733228@itesm.mx>
2021-03-01 18:36:38 -0600

    agregando componente Karen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import {
   AgmCoreModule
 } from '@agm/core';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { KarenComponent } from './karen/karen.component';
 
 @NgModule({
   imports: [
@@ -39,6 +40,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   declarations: [
     AppComponent,
     AdminLayoutComponent,
+    KarenComponent,
 
   ],
   providers: [],
